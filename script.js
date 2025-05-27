709c9f8eefa8c884b7e0a671b4ce55a680169de5
cubeofmeat-dev <cubeofmeat@proton.me>
2025-05-27 18:13:41 -0400

    making some big changes

---
+++ script.js
@@ -0,0 +1,12 @@
+function handleToMain() {
+    window.alert('To Main!');
+}
+
+function handleToGames() {
+    window.alert('To Games!');
+}
+
+function handleToOther() {
+    window.alert('To Other!');
+}
+
